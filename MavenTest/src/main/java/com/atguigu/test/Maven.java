bd34c5b1425f2e7c2260895862818a11158bce9a
linghuxiaobu <zyc01_job@163.com>
2017-08-03 12:06:11 +0800

    MavenTest2

---
+++ MavenTest/src/main/java/com/atguigu/test/Maven.java
@@ -0,0 +1,25 @@
+package com.atguigu.test;
+
+import org.apache.log4j.Logger;
+
+public class Maven {
+	private static final Logger logger = Logger.getLogger(Maven.class);
+	public static void main(String[] args) {
+		/*logger.debug("hello-----debug");
+		logger.info("hello------info");
+		logger.warn("hello------warn");
+		logger.error("hello------error");*/
+		try{
+		logger.debug("1111111");
+		int age = 5/0;
+		logger.debug("222222222");
+		logger.debug("33333333333444444");
+		logger.debug("55555555555555555555555555");
+		logger.debug("田伯光到此一游!");
+		}catch(Exception e){
+			e.printStackTrace();
+			logger.error("出错了！！！");
+			logger.error(e.getMessage(),e.getCause());
+		}
+	}
+}
